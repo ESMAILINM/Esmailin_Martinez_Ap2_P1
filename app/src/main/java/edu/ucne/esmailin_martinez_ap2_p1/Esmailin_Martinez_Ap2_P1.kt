12e4352cb24c37f1c1c8ebd2b12a868403d7dbde
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-02 18:17:10 -0400

    Agrega Hilt a la clase Aplication

--- app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/Esmailin_Martinez_Ap2_P1.kt
+++ app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/Esmailin_Martinez_Ap2_P1.kt
@@ -1,6 +1,8 @@
 package edu.ucne.esmailin_martinez_ap2_p1
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Esmailin_Martinez_Ap2_P1 : Application() {
 }
--- app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/MainActivity.kt
@@ -3,45 +3,29 @@ package edu.ucne.esmailin_martinez_ap2_p1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.esmailin_martinez_ap2_p1.navigation.EntradaHuacalesNavHost
 import edu.ucne.esmailin_martinez_ap2_p1.ui.theme.Esmailin_Martinez_Ap2_P1Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Esmailin_Martinez_Ap2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    EntradaHuacalesNavHost(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Esmailin_Martinez_Ap2_P1Theme {
-        Greeting("Android")
     }
 }
