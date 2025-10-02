9be080944c43a06b402f6326088791e1cff0a234
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-02 18:17:27 -0400

    Agrega la dependencia de Material 3 Icons Extended para el icono de filtro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation ("com.google.dagger:hilt-android:2.57.1")
     ksp("com.google.dagger:hilt-android-compiler:2.57.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    //Material 3
+    implementation("androidx.compose.material:material-icons-extended:1.3.2")
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.moshi:moshi-kotlin:1.14.0")
