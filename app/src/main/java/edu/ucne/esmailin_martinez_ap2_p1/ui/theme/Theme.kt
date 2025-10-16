651d7a8c98be0f21976a9cf2bff95e73403be3fb
ESMAILINM <esmailinmartinez05@gmail.com>
2025-10-16 18:56:17 -0400

    Desactiva el color dinámico en el tema.

--- app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/ui/theme/Theme.kt
+++ app/src/main/java/edu/ucne/esmailin_martinez_ap2_p1/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package edu.ucne.esmailin_martinez_ap2_p1.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -37,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun Esmailin_Martinez_Ap2_P1Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
